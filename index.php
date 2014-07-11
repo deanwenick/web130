77ab54b8d951feaec41a89d399fc56befb5a4db7
Forrest McIntyre <forrest.mcintyre@gmail.com>
2014-07-11 01:08:18 +0000

    exercise 2 fixed

--- index.php
+++ index.php
@@ -22,8 +22,8 @@
         </li>
       </ul>
     </nav>   
-  
-    <div id="main">
+    <div class="clear"></div>
+    <div id="left">
       <p>Lorem ipsum dolor sit amet, nunc tristique porta nulla vivamus molestie nonummy. Eros suspendisse nec commodo eleifend nec. Donec felis voluptate. Habitasse in tortor dui in hendrerit egestas, quam sit, nunc pharetra. Interdum vestibulum hymenaeos risus est gravida, porttitor eu ut, mollis nonummy aut eget dolor posuere, mus nec. Libero eget vulputate velit eleifend aenean. Quam luctus sit enim lacus accumsan, est neque, quis ut a proin ultricies pede. Rutrum molestie, id ut eget iaculis malesuada. Proin eros ut rhoncus, repellat enim, fusce aliquam phasellus, tellus assumenda dui vel pellentesque leo ut. Odio tincidunt ut, vivamus mus, aliquam quisquam turpis neque dolor elit sem, augue suspendisse iaculis. Consequat nostra vitae lacinia eu convallis, mi sit nascetur ultricies, nec sed justo sed metus.</p>
     </div>
     <div id="side">
