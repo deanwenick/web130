ae5ab0303abb5e0b3b50e9ecbfcbeb2ca2503aa6
Forrest McIntyre <forrest.mcintyre@gmail.com>
2014-07-22 23:13:59 +0000

    added responsive menu part 2

--- classwork/04.1-responsive-menu/js/scripts.js
+++ classwork/04.1-responsive-menu/js/scripts.js
@@ -7,24 +7,32 @@ $(document).ready(function(){
   });
   
   $(".nav li a").each(function() {
-    if ($(this).next().length > 0 ) {
+    if ( $(this).next().length > 0 ) {
       $(this).addClass("parent");
     }
   });
   
-   if (ww < 800) {
-    $(".toggleMenu").css("display","inline-block");
+    if (ww < 800) {
+    $(".toggleMenu").css("display", "inline-block");
     $(".nav").hide();
     $(".nav li a").click(function() {
       $(this).parent("li").toggleClass("hover");
     });
   } else {
-    $("toggleMenu").css("display","none");
+    $("toggleMenu").css("display", "none");  
     $(".nav li").hover(function() {
       $(this).addClass("hover");
     }, function() {
       $(this).removeClass("hover");
     });
-  }  
+   } 
+ 
+});//end document.ready function
 
-}); //end document.ready function
+
+
+
+
+
+
+
