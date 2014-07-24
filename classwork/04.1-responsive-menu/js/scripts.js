826c6af429c12fdf02286baf382b8a3ef53045a3
Dean Wenick <deanwenick@gmail.com>
2014-07-24 20:08:16 +0000

    update bootstrap lesson

--- classwork/04.1-responsive-menu/js/scripts.js
+++ classwork/04.1-responsive-menu/js/scripts.js
@@ -1,8 +1,11 @@
+var ww = document.body.clientWidth;
+
 $(document).ready(function(){
-  var ww = document.body.clientWidth;
-  
+
   $(".toggleMenu").click(function(e) {
     e.preventDefault();
+    //add active class for styling
+    $(this).toggleClass('active');
     $(".nav").toggle();
   });
   
@@ -12,22 +15,75 @@ $(document).ready(function(){
     }
   });
   
-    if (ww < 800) {
+  adjustMenu();
+
+ 
+});//end document.ready function
+
+// call function on page load and on window resize
+var adjustMenu = function() {
+  if (ww < 800) {
     $(".toggleMenu").css("display", "inline-block");
-    $(".nav").hide();
-    $(".nav li a").click(function() {
+
+    
+    //add condition to show/hide btn if btn active
+    if ( !$(".toggleMenu").hasClass("active") ) {
+      $(".nav").hide();
+    } else {
+      $(".nav").show();
+    }
+    
+    
+    
+    //unbind events on small screen
+    $(".nav li").unbind('mouseenter mouseleave');
+    
+    $(".nav li a.parent").unbind('click').bind('click', function(e) {
+      //attached to anchor element to prevent bubbling
+      e.preventDefault();
       $(this).parent("li").toggleClass("hover");
     });
   } else {
     $("toggleMenu").css("display", "none");  
-    $(".nav li").hover(function() {
-      $(this).addClass("hover");
-    }, function() {
-      $(this).removeClass("hover");
+    
+    //show nav on resize
+    $(".nav").show();
+    
+//     $(".nav li").hover(function() {
+//       $(this).addClass("hover");
+//     }, function() {
+//       $(this).removeClass("hover");
+      
+    $(".nav li").removeClass("hover");
+    $(".nav li a").unbind('click');
+    $(".nav li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function(){
+      $(this).toggleClass('hover');
     });
+
    } 
- 
-});//end document.ready function
+};//end adjustMenu
+
+//call adjustMenu() on window resize
+$(window).bind('resize orienationchange', function() {
+  ww = window.body.clientWidth;
+  adjustMenu();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ classwork/04.2-responsive-menu/README.md
@@ -0,0 +1,11 @@
+Responsive menu
+=======
+
+#### From [tutsplus] (http://webdesign.tutsplus.com/tutorials/big-menus-small-screens-responsive-multi-level-navigation--webdesign-8452)
+
+[down arrow image] (http://wenick-web.com/responsive-menu/downArrow.png) for the nav
+
+1. download image to desktop
+2. upload to nitrous, it will be in your root directory
+3. make an images directory in 04-responsive-menu
+4. `mv ~/downArrow.png ~/workspace/www/classwork/04-responsive-menu/images` will move the file, or you can drag it
